768890bb75ee70c9c9704e152aac818d9eb611fb
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2019-08-16 12:54:20 +0600

    DB context

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -19,8 +20,12 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(options => 
+                options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection"))
+            );
+
             services.AddMvc().AddXmlSerializerFormatters();
-            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+            services.AddTransient<IEmployeeRepository, MockEmployeeRepository>();
         }  
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
